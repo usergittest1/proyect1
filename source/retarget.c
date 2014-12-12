404d35862eb2d034cb9a24b6f11a01e937645bbb
Manuel Limones <limonesu.me@gmail.com>
2014-12-12 03:45:54 -0600

    Implement ITM retargeting

--- source/retarget.c
+++ source/retarget.c
@@ -1,1 +1,31 @@
-#include <stdio.h>
+#include <stdio.h> 
+/* Replace next line: Include the device system header file here */
+//#error "device include file missing"
+/* e.g.: #include "STM32F2xx.h" */
+#include "stm32f4xx.h"                  // Device header
+ 
+//#pragma import(__use_no_semihosting_swi)
+
+
+volatile int ITM_RxBuffer = ITM_RXBUFFER_EMPTY;  /*  CMSIS Debug Input        */
+
+struct __FILE{
+	void * handler;
+};
+
+FILE __stdout={(void*)0};
+FILE __stdin={(void*)0};
+
+int fputc(int c, FILE *f) {
+  return (ITM_SendChar(c));
+}
+
+int fgetc(FILE *f) {
+  while (ITM_CheckChar() != 1) __NOP();
+  return (ITM_ReceiveChar());
+}
+
+int ferror(FILE *f) {
+  /* Your implementation of ferror */
+  return EOF;
+}
